0ca810390742d29cf874de4fa5d6b1bb99e9b414
Mark Halls <mark.halls@gmail.com>
2019-11-01 11:02:36 -0700

    Add dark mode hook

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import PlayerList from "./components/PlayerList";
+import Nav from "./components/Nav";
+
 import { useLocalStorage } from "./hooks/useLocalStorage";
+import { useDarkMode } from "./hooks/useDarkMode";
+
+import "./App.css";
 
 function App() {
   const [playerData, setPlayerData] = useLocalStorage("playerData", []);
+  const [darkMode, setDarkMode] = useDarkMode(true);
+
+  useEffect(() => {
+    document.querySelector(".App").classList.toggle("dark");
+  }, [darkMode]);
+
+  const updateDarkMode = () => setDarkMode(!darkMode);
 
   return (
     <div className="App">
+      <Nav toggleDark={updateDarkMode} />
       <PlayerList playerData={playerData} setPlayerData={setPlayerData} />
     </div>
   );
